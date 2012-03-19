273b85ffaf56836cc864d473682ad202a9682b9a
James Hu <james.hu.ustc@hotmail.com>
2012-03-19 23:51:01 +0000

    Added CharsetFilter which can alter the charset attribute in response
    git-svn-id: https://svn.code.sf.net/p/jabb/code/trunk/code/jabb-core@98 16805ebf-6927-4a25-9f7e-4a1c404e4a5e

---
+++ src/net/sf/jabb/util/web/CharsetFilter.java
@@ -0,0 +1,61 @@
+/*
+Copyright 2012 James Hu
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package net.sf.jabb.util.web;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * The filter to alter charset of response.
+ * @author James Hu
+ *
+ */
+public class CharsetFilter implements Filter {
+	
+	protected String charset;
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.Filter#destroy()
+	 */
+	@Override
+	public void destroy() {
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
+	 */
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		response.setCharacterEncoding(charset);
+		chain.doFilter(request, response);
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+	 */
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+		charset = config.getInitParameter("charset");
+	}
+
+}
