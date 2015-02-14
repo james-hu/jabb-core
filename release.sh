7155f858c3fcf808d1331aae647b69ba2913865d
James Hu <james.hu.ustc@gmail.com>
2015-02-15 00:53:36 +1100

    added a release script

---
+++ release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+CURRENT_VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }'`
+TIMESTAMP=`date -u +%Y%m%d%k%M`
+RELEASE_VERSION=${CURRENT_VERSION%%SNAPSHOT}$TIMESTAMP
+
+echo "Switching to release branch: $CURRENT_VERSION -> $RELEASE_VERSION"
+git checkout -b "release/$RELEASE_VERSION"
+
+mvn versions:set "-DnewVersion=$RELEASE_VERSION" -DgenerateBackupPoms=false
+git commit -m "Update version number for release"
+mvn -DskipTests clean package install deploy
+git tag -l "release/v$RELEASE_VERSION"
+git push origin --tags
+
+echo "Switching back to master branch"
+git checkout master
