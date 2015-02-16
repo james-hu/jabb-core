f1ff4c61a6ac4a34e6a3a44754114fa19933e41e
James Hu <james.hu.ustc@hotmail.com>
2015-02-16 22:20:12 +1100

    fix the deletion of gpg key

--- release.sh
+++ release.sh
@@ -18,7 +18,7 @@ git tag -l "release/v$RELEASE_VERSION"
 
 mvn -DskipTests clean deploy
 
-gpg --batch --delete-secret-keys james.hu.ustc@hotmail.com `gpg --list-secret-keys --with-colons --fingerprint  james.hu.ustc@hotmail.com|sed -n 's/^fpr:::::::::\([[:alnum:]]\+\):/\1/p'`
+gpg --batch --delete-secret-keys `gpg --list-secret-keys --with-colons --fingerprint  james.hu.ustc@hotmail.com|sed -n 's/^fpr:::::::::\([[:alnum:]]\+\):/\1/p'`
 cat ~/.m2/settings.xml | sed 's/<servers>.*<\/settings>/<\/settings>/g' > settings.xml
 cp settings.xml ~/.m2/settings.xml
 rm settings.xml
