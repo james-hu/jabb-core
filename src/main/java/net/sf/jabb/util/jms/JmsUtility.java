fc1e45e229b34c8ba35458a6c1d7434b829c4716
James Hu <james.hu.ustc@hotmail.com>
2015-08-19 19:25:49 +1000

    add JmsUtility

---
+++ src/main/java/net/sf/jabb/util/jms/JmsUtility.java
@@ -0,0 +1,123 @@
+/**
+ * 
+ */
+package net.sf.jabb.util.jms;
+
+import javax.jms.Connection;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.QueueBrowser;
+import javax.jms.Session;
+
+/**
+ * Utility methods for JMS
+ * @author James Hu
+ *
+ */
+public class JmsUtility {
+	
+	/**
+	 * Generate a single line String consisted of the summary of a JMSException
+	 * @param e		the exception
+	 * @return		summary of the exception in one line
+	 */
+	static protected String exceptionSummary(JMSException e){
+		Throwable cause = e.getCause();
+		Exception linked = e.getLinkedException();
+		return "JMSException: " + e.getMessage() 
+				+ ", error code: " + e.getErrorCode()
+				+ ", linked exception: " 
+				+ (linked == null ? null : (linked.getClass().getName() + " - " + linked.getMessage()))
+				+ ", cause: " 
+				+ (cause == null ? null : (cause.getClass().getName() + " - " + cause.getMessage()));
+	}
+	
+
+	static public void closeSilently(MessageProducer sender, MessageConsumer consumer, Session session){
+		if (sender != null){
+			try{
+				sender.close();
+			}catch(Exception e){
+				// ignore
+			}
+		}
+		if (consumer != null){
+			try{
+				consumer.close();
+			}catch(Exception e){
+				// ignore
+			}
+		}
+		if (session != null){
+			try{
+				session.close();
+			}catch(Exception e){
+				// ignore
+			}
+		}
+	}
+
+	static public void closeSilently(MessageConsumer consumer, Session session){
+		if (consumer != null){
+			try{
+				consumer.close();
+			}catch(Exception e){
+				// ignore
+			}
+		}
+		if (session != null){
+			try{
+				session.close();
+			}catch(Exception e){
+				// ignore
+			}
+		}
+	}
+
+	static public void closeSilently(QueueBrowser browser, Session session){
+		if (browser != null){
+			try{
+				browser.close();
+			}catch(Exception e){
+				// ignore
+			}
+		}
+		if (session != null){
+			try{
+				session.close();
+			}catch(Exception e){
+				// ignore
+			}
+		}
+	}
+	
+	static public void closeSilently(Session session, Connection conn){
+		if (session != null){
+			try{
+				session.close();
+			}catch(Exception e){
+				// ignore
+			}
+		}
+		if (conn != null){
+			try{
+				conn.close();
+			}catch(Exception e){
+				// ignore
+			}
+		}
+	}
+	
+	static public void closeSilently(Connection conn){
+		if (conn != null){
+			try{
+				conn.close();
+			}catch(Exception e){
+				// ignore
+			}
+		}
+	}
+
+
+}
