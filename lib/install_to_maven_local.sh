f358b1ed9951750b737fb2517020c76ca55f19f8
James Hu <james.hu.ustc@hotmail.com>
2015-09-19 09:17:57 +1000

    still have to keep ojdbc14.jar in lib/

--- lib/install_to_maven_local.sh
+++ lib/install_to_maven_local.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+mvn -q install:install-file -Dfile=ojdbc14.jar -DgroupId=com.oracle -DartifactId=ojdbc14 -Dversion=10.2.0.1.0 -Dpackaging=jar
+
 mvn -q install:install-file -Dfile=magnolia-core-5.2.jar -Dpackaging=jar -DgroupId=info.magnolia -DartifactId=magnolia-core -Dversion=5.2
 mvn -q install:install-file -Dfile=magnolia-jaas-5.2.jar -Dpackaging=jar -DgroupId=info.magnolia -DartifactId=magnolia-jaas -Dversion=5.2
 
