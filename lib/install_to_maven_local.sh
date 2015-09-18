c4020e8366c584095ccbafcfa3a82829a13df445
James Hu <james.hu.ustc@hotmail.com>
2015-09-19 09:11:00 +1000

    Add DenyAccessToLocalPortFilter, and moved some dependencies from lib/ to pom.xml

--- lib/install_to_maven_local.sh
+++ lib/install_to_maven_local.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-mvn -q install:install-file -Dfile=jta-1.0.1B.jar -DgroupId=javax.transaction -DartifactId=jta -Dversion=1.0.1B -Dpackaging=jar
-mvn -q install:install-file -Dfile=ojdbc14.jar -DgroupId=com.oracle -DartifactId=ojdbc14 -Dversion=10.2.0.1.0 -Dpackaging=jar
-mvn -q install:install-file -Dfile=mmseg4j-core-1.8.4-with-dic.jar -DgroupId=com.chenlb -DartifactId=mmseg4j-core -Dversion=1.8.4-with-dic -Dpackaging=jar
-
 mvn -q install:install-file -Dfile=magnolia-core-5.2.jar -Dpackaging=jar -DgroupId=info.magnolia -DartifactId=magnolia-core -Dversion=5.2
 mvn -q install:install-file -Dfile=magnolia-jaas-5.2.jar -Dpackaging=jar -DgroupId=info.magnolia -DartifactId=magnolia-jaas -Dversion=5.2
 
