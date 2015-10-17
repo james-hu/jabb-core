1d4c465a58b506bb678b1442164b606f0f16372e
James Hu <james.hu.ustc@hotmail.com>
2015-10-17 11:58:46 +1100

    minor refactorying of LogbackRestServiceImpl

--- src/main/java/net/sf/jabb/spring/service/LogbackResetServiceImpl.java
+++ src/main/java/net/sf/jabb/spring/service/LogbackResetServiceImpl.java
@@ -6,8 +6,10 @@ package net.sf.jabb.spring.service;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.util.ContextInitializer;
+
 import org.springframework.beans.factory.BeanNameAware;
 
 /**
@@ -26,18 +28,19 @@ public class LogbackResetServiceImpl implements LogbackResetService, BeanNameAwa
 	public void resetLogback() {
 		logger.debug("Resetting Logback...");
 		ILoggerFactory lcObject = LoggerFactory.getILoggerFactory();
-		if(!(lcObject instanceof LoggerContext)) {
-			logger.error("Expected LOGBACK binding with SLF4J, but another log system has taken the place: {}", lcObject);
+		if(lcObject instanceof LoggerContext) {
+			try {
+				LoggerContext lc = (LoggerContext) lcObject;
+				lc.reset();
+				ContextInitializer ci = new ContextInitializer(lc);
+				ci.autoConfig();
+			} catch(Exception e) {
+				logger.error("Failed to reset Logback", e);
+			}	
+		    logger.info("Logback has been reset.");
+		}else{
+			logger.error("Expected LOGBACK binding with SLF4J, but another log system has taken the place: " + lcObject.getClass().getSimpleName());
 		}
-		try {
-			LoggerContext lc = (LoggerContext) lcObject;
-			lc.reset();
-			ContextInitializer ci = new ContextInitializer(lc);
-			ci.autoConfig();
-		} catch(Exception e) {
-			logger.error("Failed to reset Logback", e);
-		}	
-	    logger.info("Logback has been reset.");
 	}
 
 	@Override
