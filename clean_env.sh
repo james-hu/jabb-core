953186fc2a2accf33a024ee2866aca2c28991945
James Hu <james.hu.ustc@hotmail.com>
2016-08-25 22:12:38 +1000

    remove outdated documents

--- clean_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -o nounset
-
-gpg --batch --delete-secret-keys `gpg --list-secret-keys --with-colons --fingerprint  james.hu.ustc@hotmail.com|sed -n 's/^fpr:::::::::\([[:alnum:]]\+\):/\1/p'`
-cat ~/.m2/settings.xml | sed 's/<servers>.*<\/settings>/<\/settings>/g' > settings.xml
-cp settings.xml ~/.m2/settings.xml
-rm settings.xml
-
